e25bc3d19f502e5376de8243fdd2b71a7c5216a3
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2020-12-11 00:07:54 -0800

    First commit of rebranded Jammming project with modified stylesheets

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(e) {
+        this.setState({ term: e.target.value });
+        this.search();
+    }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist"
+                        type="text"
+                        value={this.state.term}
+                        onChange={this.handleTermChange} />
+                {/*<button className="SearchButton"
+                        onClick={this.search}>
+        FIND SONG</button>*/}
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
